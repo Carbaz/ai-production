9d084b75c1d387ad820f87904d981540be4fa414
Carlos Bazaga <cabazaga@gmail.com>
2026-06-30 23:14:11 +0200

    updated model.

--- week1/Carbaz/instant.py
+++ week1/Carbaz/instant.py
@@ -25,13 +25,15 @@ def welcome():
     explaining that it is live on production for the first time!
     """
     messages = [{"role": "user", "content": message}]
-    response = client.chat.completions.create(model="gpt-5-nano", messages=messages)
+    response = client.chat.completions.create(model="gpt-5.4-mini", messages=messages)
     reply = response.choices[0].message.content.replace("\n", "<br/>")
     html = f"""<html>
         <head>
             <title>Live in an Instant!</title>
             <meta name="color-scheme" content="light dark">
         </head>
-        <body><p>{reply}</p></body></html>
+        <body>
+            <p>{reply}</p>
+        </body></html>
     """
     return html
